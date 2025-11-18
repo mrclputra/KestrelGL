f3c2b876a105404d5faef3fa5a99ba0c97c94c41
Marcel Putra <marcelino.ares.putra@gmail.com>
2025-11-18 11:01:57 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# KestrelGL
+
+lightweight C++ OpenGL framework for loading and viewing 3D models
+uses ASSIMP and ImGUI
+
+vcpkg + cmake
+
+made this project for the learning experience
---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello Cmake" << std::endl;
+  std::cout << "C++ Standard " << __cplusplus << std::endl;
+  std::cout << "KestrelGL" << std::endl;
+
+  // initialize and configure GLFW
+
+  // create window
+
+  // load OpenGL GLAD function pointers
+  
+  // build and compile shader program
+
+  // load models
+
+  // setup scene
+
+  // render loop
+
+  // cleanup
+
+  return 0;
+}
