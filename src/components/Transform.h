a8d9fdf2829bee83b031583eb2282852ad79611f
Marcel Putra <marcelino.ares.putra@gmail.com>
2026-01-17 23:29:50 +0800

    glm convention fix

--- src/components/Transform.h
+++ src/components/Transform.h
@@ -22,9 +22,9 @@ public:
         if (isDirty) {
             modelMatrix = glm::mat4(1.0f);
             modelMatrix = glm::translate(modelMatrix, position);
-            modelMatrix = glm::rotate(modelMatrix, glm::radians(rotation.x), { 1, 0, 0 });
-            modelMatrix = glm::rotate(modelMatrix, glm::radians(rotation.y), { 0, 1, 0 });
-            modelMatrix = glm::rotate(modelMatrix, glm::radians(rotation.z), { 0, 0, 1 });
+            modelMatrix = glm::rotate(modelMatrix, glm::radians(rotation.x), glm::vec3(1.0, 0.0, 0.0));
+            modelMatrix = glm::rotate(modelMatrix, glm::radians(rotation.y), glm::vec3(0.0, 1.0, 0.0));
+            modelMatrix = glm::rotate(modelMatrix, glm::radians(rotation.z), glm::vec3(0.0, 0.0, 1.0));
             modelMatrix = glm::scale(modelMatrix, scale);
 
             isDirty = false;
